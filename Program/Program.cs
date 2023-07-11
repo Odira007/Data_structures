3cda8ed697f7db3b1b3cb534b97e9ae9c4c61a32
Odira Ikewelugo <odiraikewelugo@gmail.com>
2023-07-11 18:59:47 +0100

    Data Structures

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using static System.Console;
 using DataStructures.MyStack;
+using DataStructures.MyQueue;
 
 class Program
 {
     static void Main()
     {
-        
+        Stack stack = new Stack();
+        Queue queue = new Queue();
+        queue.Enqueue(3);
+        queue.Enqueue(3);
+        queue.Enqueue(3);
+        queue.Enqueue(3);
+        WriteLine(queue.Size());
+        WriteLine(queue.IsEmpty());
+        queue.Dequeue();
+        queue.Dequeue();
+        WriteLine(queue.Size());
+
     }
 }
